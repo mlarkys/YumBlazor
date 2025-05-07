e715beb5838be122560f835cf80ef630e60bc6cf
Michael Lacroix <mlacroix@arkys.com>
2025-05-07 15:24:52 -0400

    Add Toastr and a lot more

---
+++ wwwroot/lib/js/Notification.js
@@ -0,0 +1,12 @@
+﻿window.ShowToastr = function (type, message) {
+    if (type == "success")
+        toastr.success(message);
+    else if (type == "info")
+        toastr.info(message);
+    else if (type == "warning")
+        toastr.warning(message);
+    else if (type == "error")
+        toastr.error(message);
+    else
+        toastr.info("Unknown message type: " + type);
+}
